6e5ae53d2dee3debb5b1afa6eb889f5216cc01f9
Jim Charnesky <mijnesky@hotmail.com>
2021-02-18 09:36:51 -0700

    Add more mantras and affirmations

--- main.js
+++ main.js
@@ -13,19 +13,27 @@ var affirmations = [
   "I'm against picketing, but I don't know how to show it.",
   "If my kid couldn't draw I'd make sure that my kitchen magnets didn't work.",
   "When someone hands you a flyer, it's like they're saying here you throw this away.",
-  "My belt holds my pants up, but the belt loops hold my belt up. So which one is the real hero?"
+  "My belt holds my pants up, but the belt loops hold my belt up. So which one is the real hero?",
+  "I saw a human pyramid once. It was very unnecessary.",
+  "Fettucini alfredo is macaroni and cheese for adults.",
+  "I haven't slept for ten days, because that would be too long.",
+  "All these jokes have been pre-approved as funny by me."
 ];
 var mantras = [
-  "It's weird... people say they're not like apes. Now how do you explain football then?",
   "My fake plants died because I did not pretend to water them.",
   "The depressing thing about tennis is that no matter how good I get, I'll never be as good as a wall.",
   "Dogs are forever in the push up postion.",
   "I wanted to buy a candle holder, but the store didn't have one. So I got a cake.",
+  "It's weird... people say they're not like apes. Now how do you explain football then?",
   "I'm sick of following my dreams. I'm just going to ask them where they're going and hook up with them later.",
   "Every book is a children's book if the kid can read!",
   "I remixed a remix, it was back to normal.",
   "I think foosball is a combination of soccer and shish kabobs.",
   "I'm gonna fix that last joke by taking out all the words and adding new ones.",
   "If carrots got you drunk, rabbits would be messed-up.",
-  "A severed foot is the ultimate stocking stuffer."
+  "A severed foot is the ultimate stocking stuffer.",
+  "I'd like to get four people who do cart wheels very good, and make a cart.",
+  "I like Kit-Kat, unless I'm with four or more people.",
+  "This shirt is dry clean only. Which means... it's dirty.",
+  "Rice is great if you're really hungry and want to eat two thousand of something."
 ];
