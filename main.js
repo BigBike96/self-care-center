9b720b7445870c5ca790986fda749183a35e4070
Jim Charnesky <mijnesky@hotmail.com>
2021-02-17 21:12:45 -0700

    Add querySelectors for buttons

---
+++ main.js
@@ -0,0 +1,3 @@
+var affirmationButton = document.querySelector('#affirmation');
+var mantraButton = document.querySelector('#mantra');
+var receiveMessageButton = document.querySelector('#receive-message');
